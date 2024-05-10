c6c34d1a5c712528de89d73810693afc96a222d7
Enes Burak <96229656+EnesBur4k@users.noreply.github.com>
2024-05-10 16:30:51 +0300

    Add files via upload

---
+++ proje/include/linkedlist.h
@@ -0,0 +1,24 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+// Bağlı listenin düğüm yapısı
+struct Node {
+    char data;
+    struct Node* next;
+    struct Node* prev; // Yeni bir 'prev' işaretçisi ekliyoruz
+};
+
+// Yeni düğüm oluşturma fonksiyonu
+struct Node* createNode(char data);
+
+// Bağlı listeye eleman ekleme fonksiyonu
+void insert(struct Node** headRef, char data);
+
+// Bağlı listeyi yazdırma fonksiyonu
+void printList(struct Node* node);
+
+// Bağlı listeden eleman silme fonksiyonu
+void deleteNode(struct Node* delNode);
+
+#endif
+
